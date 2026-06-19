00ecde4e96e1362b5b147acf2b10ae1833ff42d0
brendaalcantara-cell <brenda.alcantara@escola.pr.gov.br>
2026-06-19 10:28:11 +0000

    iyviuvy

---
+++ JOGO.py
@@ -0,0 +1,9 @@
+print(**********************************)
+print("Bem vindo ao jogo de Adivinhação")
+print(**********************************)
+
+numero_secreto= 26
+
+chute=input("Digite o seu número:")
+
+# print (chute)
