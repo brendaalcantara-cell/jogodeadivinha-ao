491f2aa8e16219dbf551a816f47c843e046946ee
brendaalcantara-cell <brenda.alcantara@escola.pr.gov.br>
2026-06-25 14:12:43 +0000

    jogo

--- JOGO.py
+++ JOGO.py
@@ -6,4 +6,15 @@ numero_secreto= 26
 
 chute=input("Digite o seu número:")
 
-# print (chute)
+# print ("você digitou: ",chute )
+chutenumerico= int (chute)
+
+acertou= chutenumerico= numero_secreto
+maior=chutenumerico> numero_secreto
+menor+chutenumerico<numero_secreto
+
+if(numero_secreto= chutenumerico):
+    print("você acertou!")
+else:
+    print("você errou")
+    print("fim de jogo")
